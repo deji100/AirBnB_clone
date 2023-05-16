f98a7da1ec090d6107c33db7cfe8102a90f15032
deji100 <adeyemidej9@gmail.com>
2023-05-16 13:42:57 -0700

    .

---
+++ models/base_model.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Defines a BaseModel class inherited by other subclasses"""
+from uuid import uuid4
+from datetime import datetime
+from models import storage
+
+
+class BaseModel:
+    """A base class for all subclasses to inherit from"""
+
+    def __init__(self, *args, **kwargs):
+        """Initializes a new instance of the BaseModel class"""
+
+        if not kwargs:
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            for key, value in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    setattr(self, key, datetime.fromisoformat(value))
+                else:
+                    setattr(self, key, value)
+
+    def __str__(self):
+        """Returns the string rep of an instance"""
+
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
+    
+    def save(self):
+        """
+        Updates self.updated_at with the current datetime
+        """
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__"""
+
+        obj_dict = self.__dict__.copy()
+        setattr(self, "__class__", type(self).__name__)
+        setattr(self, obj_dict["created_at"],
+                obj_dict["created_at"].isoformat())
+        setattr(self, obj_dict["updated_at"],
+                obj_dict["updated_at"].isoformat())
+
+        return obj_dict
---
+++ models/engine/file_storage.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""Defines the FileStorage class model"""
+import os
+import json
+
+
+class FileStorage:
+    """A class for serializing or storing data and 
+    deserializing or retrieving data."""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the all objs"""
+        return self.__objects
+    
+    def new(self, obj):
+        """
+        Sets a new obj in __objects with key <obj class name>.id
+        """
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+
+    def save(self):
+        """Serializes __objects to a JSON file `file.json`"""
+        with open(self.__file_path, mode="w") as f:
+            storage = {}
+            for k, v in self.__objects.items():
+                storage[k] = v.to_dict()
+            f.write(json.dumps(storage))
+
+    def reload(self):
+        """Deserializes a JSON file to __objects"""
+
+        if not os.path.isfile(FileStorage.__file_path):
+            return
+        
+        with open(self.__file_path, encoding="utf-8") as f:
+            storage = json.load(f)
+            for obj in storage.values():
+                self.new(eval(obj["__class__"])(**obj))
